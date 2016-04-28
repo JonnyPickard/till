63c36841d75934a6cc6421bd491a095dde7d0d64
Jonny Pickard <jonathandpickard@gmail.com>
2016-04-28 16:34:08 +0100

    Added add to menu function

--- src/interface.js
+++ src/interface.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 
-  var till = new Till();
-  var menu = new Menu();
-  var obj = menu.returnFoodList();
+  till = new Till();
+  menu1 = new Menu();
+
+  var obj = menu1.returnFoodList();
   var list = $('<ul />');
 
   extractResult(obj);
@@ -14,4 +15,19 @@ $(document).ready(function() {
         $('<li />', {text: index + ': \xA3' + value}).appendTo(list);
     });
   }
+
+  $('#addItem').submit(function(event) {
+    event.preventDefault();
+    var food = $('#food').val();
+    var price = $('#price').val();
+    if(Math.floor(price) == price && $.isNumeric(price)) {
+      // guard clause to only allow integers as prices
+      menu1.addItem(food, price);
+      // add new item as LI to list
+      $('<li />', {text: food + ': \xA3' + price}).appendTo(list);
+    } else {
+      alert("This value must be numeric");
+    }
+  });
+
 });
